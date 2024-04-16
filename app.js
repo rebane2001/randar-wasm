1469b1c5254739f8f8842fb2dac98b8539bfca6d
rebane2001 <rebane2001@gmail.com>
2024-04-17 01:13:49 +0300

    cooler logging

--- app.js
+++
@@ -1,6 +0,0 @@
-Module.ccall(
-    "crackItemDropCoordinate", // name of C function
-    'number', // return type
-    ['number', 'number', 'number', ], // argument types
-    [461429.6284919679165, 61.28581428527832, -1510.2752998769283], // arguments
-  );
